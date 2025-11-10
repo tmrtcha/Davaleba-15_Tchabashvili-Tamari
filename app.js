38fb864b0fd090c6be5462fc47e60b0c3d4f96b5
tmrtcha <tchabashvili.tamari@gmail.com>
2025-11-11 00:24:00 +0400

    primitive data types app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// NUMBER
+let soldQuantity = 1500;
+let pricePerItem = 2.5;
+let totalRevenue = soldQuantity * pricePerItem;
+console.log("Total Revenue: $" + totalRevenue);
+console.log("Type _ " + typeof totalRevenue);
+
+// STRING
+let message = "Great job! You have exceeded the revenue target. Keep it up!";
+console.log(message);
+console.log("Type _ " + typeof message);
+
+// Boolean
+let isTargetMet = true;
+console.log("Revenue Target Is Met _ " + isTargetMet);
+console.log("Type _ " + typeof isTargetMet);
+
+// Undefined
+let bonusAmount;
+console.log("Bonus Amount _ " + bonusAmount);
+console.log("Type _ " + typeof bonusAmount);
+
+// Null
+let discount = null;
+console.log("Discount _ " + discount);
+console.log("Type _ " + typeof discount);
